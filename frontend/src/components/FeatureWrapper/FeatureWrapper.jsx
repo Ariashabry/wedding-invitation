616210f4b6243d7b3cfc90974e09e77bd08141ca
Alcides Ticlla <alcidesticlla@cloud.upwork.com>
2025-01-19 19:13:48 -0400

    updates ff componets to view on mobile

--- frontend/src/components/FeatureWrapper/FeatureWrapper.jsx
+++ frontend/src/components/FeatureWrapper/FeatureWrapper.jsx
@@ -1,13 +1,17 @@
 import { useFeatureFlag } from '../../hooks/useFeatureFlag';
 
-const FeatureWrapper = ({ featureKey, children }) => {
+const FeatureWrapper = ({ children, featureKey }) => {
     const isEnabled = useFeatureFlag(featureKey);
 
     if (!isEnabled) {
         return null;
     }
 
-    return children;
+    return (
+        <>
+            {children}
+        </>
+    );
 };
 
 export default FeatureWrapper; 
